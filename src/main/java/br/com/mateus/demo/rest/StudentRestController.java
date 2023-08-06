f7e6289730106748d0de8836317af26bf4c08b00
mateusf-prog <mateus102006@hotmail.com>
2023-08-06 12:31:55 -0300

    refactor StudentRestController and make Global Exception Handler

--- src/main/java/br/com/mateus/demo/rest/StudentRestController.java
+++ src/main/java/br/com/mateus/demo/rest/StudentRestController.java
@@ -42,34 +42,6 @@ public class StudentRestController {
         }
         return theStudents.get(studentId);
     }
-
-    // add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handlerException(StudentNotFoundException exc) {
-
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler ... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handlerException(Exception exc) {
-
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
 
 
